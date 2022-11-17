155d1c92f0a601ff42f82cd8dfa970628df2d70e
ianK-code <kian99564@gmail.com>
2022-11-17 16:32:21 +0300

    Will come back to it later

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Ian",
+    streetAddress: "35488825",
+}
+
+// returns an employee with the original key value 
+// pairs and the new key value pair
+function updateEmployeeWithKeyAndValue(employee, key, value){
+
+    const newObj = { ...employee }
+    newObj[key] = value
+    return newObj
+
+}
+updateEmployeeWithKeyAndValue(employee, "sam", "1")
+
+
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee.name = key
+//     employee.streetAddress = value
+//     return employee
+// }
+// destructivelyUpdateEmployeeWithKeyAndValue(employee, "Sam", "12 Broadway")
+
+// const newEmployee = nondestructivelyUpdateObject(
+//     tuesdayMenu,
+//     "Sam",
+//     "12 Broadway"
+// );
+
+
+
+// Dont know exactly what it does
+function deleteFromEmployeeByKey(employee, key){
+    const remEmployee = { ...employee }
+    let newEmployee = delete remEmployee[key]
+    return newEmployee
+}
+deleteFromEmployeeByKey(employee, "John")
